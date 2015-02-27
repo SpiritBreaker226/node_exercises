10ea85ee6a08fbe805842feeae781b97bd87f34b
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-27 16:03:38 -0500

    Add My First ASYNC IO Exercise For Learn You Node

---
+++ learnyounode/my_first_async_io.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+fs.readFile(process.argv[2], function(err, contents) {
+	if (!err) {
+		var lines = contents.toString().split('\n').length - 1;
+
+		console.log(lines);
+	}
+});
